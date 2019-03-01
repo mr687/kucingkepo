cdc79111a771c04cbe3ce21c545ff7bf7a37841c
mr687 <davinomoehdanino@gmail.com>
2019-03-01 23:27:56 +0700

    Tahap akhir

--- app/src/main/java/com/kucingkepo/kucingkepo/MainActivity.java
+++ app/src/main/java/com/kucingkepo/kucingkepo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.kucingkepo.kucingkepo;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +17,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    WebView webView;
+    String WEB_URL="https://www.kucingkepo.com/";
+    WebSettings webSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +34,32 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        setTitle("KUCINGKEPO.COM");
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(WEB_URL);
+        webView.requestFocus();
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            webView.evaluateJavascript("enable();", null);
+        } else {
+            webView.loadUrl("javascript:enable();");
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSavePassword(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setEnableSmoothTransition(true);
+        webView.setWebViewClient(new WebViewClient(){
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -40,11 +70,15 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -66,9 +100,30 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Search");
+            final EditText inputSearch = new EditText(this);
+            inputSearch.setInputType(InputType.TYPE_CLASS_TEXT);
+            alert.setView(inputSearch);
+            alert.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    webView.loadUrl(WEB_URL+"search?q=" + inputSearch.getText().toString().trim() + "&max-results=8&m=1");
+                }
+            });
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            alert.show();
             return true;
+        }
+
+        if(id == R.id.action_refresh){
+            webView.reload();
         }
 
         return super.onOptionsItemSelected(item);
@@ -80,18 +135,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if(id == R.id.nav_home){
+            webView.loadUrl(WEB_URL);
+        }
+        if(id == R.id.nav_tips_cat){
+            webView.loadUrl(WEB_URL+"search/label/TIPS");
+        }
+        if(id == R.id.nav_cat_knowledge){
+            webView.loadUrl(WEB_URL+"search/label/PENGETAHUAN");
+        }
+        if(id == R.id.nav_cat_story){
+            webView.loadUrl(WEB_URL+"search/label/CERITA%20KUCING");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
